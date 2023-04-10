01b13f67a29686d40489732aa032b7a2071b4d9c
Jananayani <abjmaheshika@gmail.com>
2023-04-10 20:32:37 +0530

    Add checkbox

--- dist/script.js
+++ dist/script.js
@@ -21,40 +21,50 @@ const handleSubmit = (e) => {
     todos.push(newTodo);
     localStorage.setItem('todos', JSON.stringify(todos));
     // APPEND TODO FN
-    appendTodo(newTodo);
+    appendTodoItem(newTodo);
     // RESET INPUT VALUE
     inputTodo.value = "";
 };
 // 6 APPEND TODOS TO DOM
 window.addEventListener("DOMContentLoaded", () => {
     const todos = JSON.parse(localStorage.getItem('todos') || '[]');
-    todos.forEach((todo) => appendTodo(todo));
+    todos.forEach((todo) => appendTodoItem(todo));
 });
 // 3 APPEND TODO FN
-const appendTodo = (newTodo) => {
+const appendTodoItem = (todo) => {
     // APPEND NEW TODO TO THE DOM
-    // Create new LI and Checkbox
+    // Create new LI, Checkbox and Buttons
     const newLi = document.createElement("li");
-    const checkB = document.createElement("input");
-    checkB.type = "checkbox";
-    checkB.checked = newTodo.completed;
-    // ADD CHECKBOX EVENT LISTENER
-    checkB.onchange = () => {
-        console.log(checkB.checked);
-        newTodo.completed = checkB.checked;
-        // SAVE CHANGES TO LOCAL STORAGE
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.onchange = () => {
+        todo.completed = checkbox.checked;
         const todos = JSON.parse(localStorage.getItem('todos') || '[]');
-        const index = todos.findIndex((t) => t.id === newTodo.id);
-        todos[index] = newTodo;
+        const index = todos.findIndex((t) => t.id === todo.id);
+        todos[index] = todo;
         localStorage.setItem('todos', JSON.stringify(todos));
+    };
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.onclick = () => {
+        const newTodo = prompt("Enter new todo text:", todo.todo);
+        if (newTodo) {
+            todo.todo = newTodo;
+            const todos = JSON.parse(localStorage.getItem('todos') || '[]');
+            const index = todos.findIndex((t) => t.id === todo.id);
+            todos[index] = todo;
+            localStorage.setItem('todos', JSON.stringify(todos));
+            newLi.childNodes[1].textContent = newTodo;
+        }
     };
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.onclick = () => {
-        deleteTodoItem(newTodo);
+        deleteTodoItem(todo);
         todoList.removeChild(newLi);
     };
-    newLi.append(newTodo.todo, checkB, deleteBtn);
+    newLi.append(todo.todo, checkbox, editBtn, deleteBtn);
     todoList === null || todoList === void 0 ? void 0 : todoList.prepend(newLi);
 };
 // 7 DELETE ALL TODOS
@@ -66,9 +76,6 @@ const clearTodos = () => {
 const deleteTodoItem = (todo) => {
     const todos = JSON.parse(localStorage.getItem('todos') || '[]');
     const index = todos.findIndex((t) => t.id === todo.id);
-    if (index !== -1) {
-        todos.splice(index, 1);
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }
+    todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 };
-btnDeleteAll.onclick = () => clearTodos();
--- src/script.ts
+++ src/script.ts
@@ -29,7 +29,7 @@ const handleSubmit = (e: Event) => {
   todos.push(newTodo);
   localStorage.setItem('todos', JSON.stringify(todos));
   // APPEND TODO FN
-  appendTodo(newTodo);
+  appendTodoItem(newTodo);
   // RESET INPUT VALUE
   inputTodo.value = "";
 };
@@ -37,41 +37,50 @@ const handleSubmit = (e: Event) => {
 // 6 APPEND TODOS TO DOM
 window.addEventListener("DOMContentLoaded", () => {
   const todos = JSON.parse(localStorage.getItem('todos') || '[]');
-  todos.forEach((todo: NewTodo) => appendTodo(todo));
+  todos.forEach((todo: NewTodo) => appendTodoItem(todo));
 });
 
 // 3 APPEND TODO FN
-const appendTodo = (newTodo: NewTodo) => {
+const appendTodoItem = (todo: NewTodo) => {
   // APPEND NEW TODO TO THE DOM
-  // Create new LI and Checkbox
+  // Create new LI, Checkbox and Buttons
   const newLi = document.createElement("li");
-  const checkB = document.createElement("input");
-  checkB.type = "checkbox";
-  checkB.checked = newTodo.completed;
 
-  // ADD CHECKBOX EVENT LISTENER
-  checkB.onchange = () => {
-    console.log(checkB.checked);
-    newTodo.completed = checkB.checked;
-    // SAVE CHANGES TO LOCAL STORAGE
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.onchange = () => {
+    todo.completed = checkbox.checked;
     const todos = JSON.parse(localStorage.getItem('todos') || '[]');
-    const index = todos.findIndex((t: NewTodo) => t.id === newTodo.id);
-    todos[index] = newTodo;
+    const index = todos.findIndex((t: NewTodo) => t.id === todo.id);
+    todos[index] = todo;
     localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.onclick = () => {
+    const newTodo = prompt("Enter new todo text:", todo.todo);
+    if (newTodo) {
+      todo.todo = newTodo;
+      const todos = JSON.parse(localStorage.getItem('todos') || '[]');
+      const index = todos.findIndex((t: NewTodo) => t.id === todo.id);
+      todos[index] = todo;
+      localStorage.setItem('todos', JSON.stringify(todos));
+      newLi.childNodes[1].textContent = newTodo;
+    }
   };
 
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
   deleteBtn.onclick = () => {
-    deleteTodoItem(newTodo);
+    deleteTodoItem(todo);
     todoList.removeChild(newLi);
   };
 
-  newLi.append(newTodo.todo, checkB, deleteBtn);
+  newLi.append(todo.todo, checkbox, editBtn, deleteBtn);
   todoList?.prepend(newLi);
 };
-
-
 
 // 7 DELETE ALL TODOS
 const clearTodos = () => {
@@ -83,14 +92,6 @@ const clearTodos = () => {
 const deleteTodoItem = (todo: NewTodo) => {
   const todos = JSON.parse(localStorage.getItem('todos') || '[]');
   const index = todos.findIndex((t: NewTodo) => t.id === todo.id);
-  if (index !== -1) {
-    todos.splice(index, 1);
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }
+  todos.splice(index, 1);
+  localStorage.setItem('todos', JSON.stringify(todos));
 };
-
-
-
-
-btnDeleteAll.onclick = () => clearTodos();
-
